fa194e5259f1c459e613339a6e198eea47bebe92
dadadadas111 <long6athcskl@gmail.com>
2025-01-28 11:55:10 +0700

    fix: UI

---
+++ Client/WidthReducerConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Client
+{
+    public class WidthReducerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double width)
+            {
+                return width * 0.9; // Reduce width to 90% of ListView
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
